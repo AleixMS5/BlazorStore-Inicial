10a4413550394a1d5f1f3044efb4871cfb77e57e
usuario <usuario@DESKTOP-5ATE2VV>
2022-11-23 12:00:50 +0100

    Add project files.

---
+++ BlazorStore/Components/Carousel/CarouselItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStore.Components.Carousel
+{
+
+    public class CarouselItem
+    {
+        public string Image { get; set; }
+        public string Text { get; set; }
+        public string Link { get; set; }
+        public CarouselItem(string src, string text, string link)
+        {
+            Image = src;
+            Text = text;
+            Link = link;
+        }
+    }
+}
---
+++ BlazorStore/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    window.initializeCarousel = function () {
+        $(".carousel").carousel({
+            interval: 5000
+        });
+    };
+
+    window.bootstrapModal = {
+        show: function (element) {
+            $(element).modal("show");
+        }
+    };
+})();
