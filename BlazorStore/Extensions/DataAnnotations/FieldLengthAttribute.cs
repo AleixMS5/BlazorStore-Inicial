093fbc92e0301fc4b7f3ba248bbf87f08a4da936
AleixMS5 <aleixmonterosabate@gmail.com>
2022-11-30 12:04:57 +0100

    login y paginas de usuario

---
+++ BlazorStore/Extensions/DataAnnotations/FieldLengthAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorStore.Extensions.DataAnnotations
+{
+    public class FieldLengthAttribute : MaxLengthAttribute
+    {
+        public FieldLengthAttribute(int maxLength) : base(maxLength)
+        {
+            ErrorMessage = "Up to {0} chars allowed";
+        }
+    }
+}
---
+++ BlazorStore/Extensions/DataAnnotations/RequiredFieldAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO.Enumeration;
+
+namespace BlazorStore.Extensions.DataAnnotations
+{
+    public class RequiredFieldAttribute : RequiredAttribute
+    {
+        public RequiredFieldAttribute()
+        {
+            ErrorMessage = "This field is required";
+        }
+    }
+}
